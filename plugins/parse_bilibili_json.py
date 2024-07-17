e7bdd07d3268e1aceed384b41bd56ba5e7553ee0
shenghuo2 <1308357113@qq.com>
2024-07-17 17:23:06 +0800

    ✨ feat(parse_bilibili_json): 支持另一种客户端的卡片消息解析
    Support for parsing card messages whose actual content is json in the “BV” format.

--- plugins/parse_bilibili_json.py
+++ plugins/parse_bilibili_json.py
@@ -139,7 +139,7 @@ async def _(event: GroupMessageEvent):
                     vd_info = await video.get_video_base_info(bvid)
 
         # https:\/\/b23.tv\/BOGZh4I
-    if "https:\/\/b23.tv" in  event.raw_message:
+    if "https:\/\/b23.tv" in event.raw_message:
         msg = event.raw_message
         url = "https://" + msg[msg.find("b23.tv") : msg.find("b23.tv") + 15].replace('\/','/')
         logger.info("小程序URL测试：",url)
@@ -151,7 +151,13 @@ async def _(event: GroupMessageEvent):
                 url = (str(response.url).split("?")[0]).strip("/")
                 bvid = url.split("/")[-1]
                 vd_info = await video.get_video_base_info(bvid)
-        
+    if "BV" in event.raw_message:
+        msg = event.raw_message
+        index = msg.find("BV")
+        if len(msg[index + 2 :]) >= 10:
+            msg = msg[index : index + 12]
+            url = f"https://www.bilibili.com/video/{msg}"
+            vd_info = await video.get_video_base_info(msg)
     if vd_info:
         if (
             url in _tmp.keys() and time.time() - _tmp[url] > 30
