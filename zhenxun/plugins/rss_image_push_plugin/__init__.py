ea25b4aef62afbb837113e328641b2342ee7e137
shenghuo2 <1308357113@qq.com>
2025-02-14 18:51:58 +0800

    fix: send image exception use URL

--- zhenxun/plugins/rss_image_push_plugin/__init__.py
+++ zhenxun/plugins/rss_image_push_plugin/__init__.py
@@ -1,6 +1,6 @@
 # main_plugin.py
 from nonebot import get_bot
-from nonebot.adapters.onebot.v11 import MessageSegment, Message
+from nonebot.adapters.onebot.v11 import MessageSegment, Message, Bot
 from nonebot_plugin_apscheduler import scheduler
 from zhenxun.services.log import logger
 from time import sleep
@@ -13,7 +13,7 @@ target_group_id = 787599185  # 目标群聊ID
 # 定义定时任务，每分钟检查一次RSS更新
 @scheduler.scheduled_job("interval", minutes=5)
 
-async def rss_image_forward_job():
+async def rss_image_forward_job(bot: Bot):
     logger.info("Checking for RSS updates...","rss_pix_push")
     try:
         anning_entries = parse_rss(rss_anning_url)  # 获取anning RSS条目
@@ -22,20 +22,20 @@ async def rss_image_forward_job():
         ManyACG_new_entries = get_new_entries_ID(ManyACG_entries)  # 获取新的条目
         # logger.info(str(ManyACG_new_entries),"rss_pix_push")
         if anning_new_entries:
-            await send_image_to_group(anning_new_entries)  # 发送新的图片到群聊
+            await send_image_to_group(anning_new_entries, bot)  # 发送新的图片到群聊
         if ManyACG_new_entries:
-            await send_image_to_group(ManyACG_new_entries)  # 发送新的图片到群聊
+            await send_image_to_group(ManyACG_new_entries, bot)  # 发送新的图片到群聊
         if anning_new_entries == [] and ManyACG_new_entries == []:
             logger.info("No new RSS entries available.")
     except Exception as e:
         logger.error(f"检查RSS更新失败: {e}")
 
 
-async def send_image_to_group(new_entries):
+async def send_image_to_group(new_entries, bot: Bot):
     """
     
     """
-    bot = get_bot()
+    # bot = get_bot()
     for entry in new_entries:
         # 提取description中的所有图片链接
          
@@ -61,6 +61,7 @@ async def send_image_to_group(new_entries):
                     logger.info(f"图片转发成功: {image_url}")
                 except Exception as e:
                     logger.error(f"发送图片时出错: {e}")
+                    await bot.send_group_msg(group_id=target_group_id, message=f"发送图片时出错: {e}\n图片URL:{image_url}")
         else:
             logger.warning(f"No images found in the new RSS entry: {entry.title}")
 
