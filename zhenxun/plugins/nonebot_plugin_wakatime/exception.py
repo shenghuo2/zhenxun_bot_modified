9dbd288d1e7a23e8b9d2f5be13d4bad42082d3bf
shenghuo2 <1308357113@qq.com>
2026-05-22 00:10:57 +0800

    refactor(WakaTime): 移除 WakaTime 插件

--- zhenxun/plugins/nonebot_plugin_wakatime/exception.py
+++
@@ -1,9 +0,0 @@
-from nonebot.exception import NoneBotException
-
-
-class UserUnboundException(NoneBotException):
-    """User Not Bound"""
-
-
-class BindUserException(NoneBotException):
-    """Bind User Api Failed"""
